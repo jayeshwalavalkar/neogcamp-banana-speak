872f4707b695412439d9adc04dc808bb8414f707
jayeshwalavalkar <jayeshwalavalkar7@gmail.com>
2022-04-17 20:22:44 +0530

    Minion translator added

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ var button = document.querySelector("#btn");
 var textArea = document.querySelector("#txt-area");
 var divOutput = document.querySelector("#div-output");
 
+var translateApiUrl = "https://api.funtranslations.com/translate/minion.json";
 
-function translate() {
+function getTransplateApi(text) {
+  return translateApiUrl + "?text=" + text;
+}
 
+function errorHandler(error) {
+  console.log("Error received from translate api" + error);
 }
 
 function onButtonClickHandler() {
-    divOutput.innerText = textArea.value;
+  fetch(getTransplateApi(textArea.value))
+    .then((response) => response.json())
+    .then((json) => (divOutput.innerText = json.contents.translated))
+    .catch(errorHandler);
 }
 
 button.addEventListener("click", onButtonClickHandler);
